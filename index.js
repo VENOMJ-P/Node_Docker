e0899685b6c4a21a2c2ce24ac5c9ba1a483babe9
VENOMJ-P <jay63837@gmail.com>
2023-09-24 12:00:31 +0530

    Added node server with docker

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.get("/",(req,res)=>{
+    console.log("updated");
+    res.send("<h1>Hello Docker</h1>")
+})
+
+const port = process.env.PORT || 3001;
 
 app.listen(port,()=>{
     console.log(`Server started at ${port}`);
